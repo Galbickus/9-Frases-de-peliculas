95385f9a71934725731f90ac9dc3addca8dc962d
Veronica <vgalbicka@gmail.com>
2024-05-17 04:23:22 -0300

    agrega query

--- frases-de-peliculas/src/main/java/com/alura/frasesdepeliculas/repository/FraseRepository.java
+++ frases-de-peliculas/src/main/java/com/alura/frasesdepeliculas/repository/FraseRepository.java
@@ -2,8 +2,9 @@ package com.alura.frasesdepeliculas.repository;
 
 import com.alura.frasesdepeliculas.model.Frase;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface FraseRepository extends JpaRepository<Frase, Long> {
-
+    @Query("SELECT f FROM Frase f order by function('RANDOM') LIMIT 1")
     public Frase obtenerFraseAleatoria();
 }
